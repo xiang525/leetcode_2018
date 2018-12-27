79d1f14f3a809ba36b36c29360e150c7cbf861f8
Emily Zuo <xiang.zuo@teradata.com>
2018-12-26 22:57:04 -0800

    ddd

---
+++ python/252.meeting_room.py
@@ -0,0 +1,28 @@
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution(object):
+    def canAttendMeetings(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: bool
+        """
+        intervals.sort(key=lambda x: x.start)
+        n = len(intervals)
+        for i in range(n-1):            
+            if intervals[i].end > intervals[i+1].start:
+                return False
+        return True
+
+
+def canAttendMeetings(self, intervals):
+    intervals.sort(key=lambda x: x.start)
+
+    for i in range(1, len(intervals)):
+        if intervals[i].start < intervals[i-1].end:
+            return False
+
+    return True
