2735341a83705568afdca4ef68b4d55be75c0c9e
Emily Zuo <xiang.zuo@teradata.com>
2019-02-06 10:17:52 -0800

    dd

--- python/59.spiral_matrix_ii.py
+++ python/59.spiral_matrix_ii.py
@@ -8,19 +8,19 @@ def generateMatrix(self, n):
         left = 0; right = len(matrix[0])-1
         direct = 0; count = 0
         while True:
-            if direct == 0:
+            if direct == 0: # go left
                 for i in range(left, right+1):
                     count += 1; matrix[up][i] = count
                 up += 1
-            if direct == 1:
+            if direct == 1: # go down
                 for i in range(up, down+1):
                     count += 1; matrix[i][right] = count
                 right -= 1
-            if direct == 2:
+            if direct == 2:  # got left
                 for i in range(right, left-1, -1):
                     count += 1; matrix[down][i] = count
                 down -= 1
-            if direct == 3:
+            if direct == 3: # go up
                 for i in range(down, up-1, -1):
                     count += 1; matrix[i][left] = count
                 left += 1
--- python/73.set_matrix_zeroes.py
+++ python/73.set_matrix_zeroes.py
@@ -37,24 +37,29 @@ class Solution:
     def setZeroes(self, matrix):
         m,n = len(matrix),len(matrix[0])
         row_zero, col_zero = False,False
+        # Does first row have zero?
         for i in range(m):
             if matrix[i][0] == 0:
                 row_zero = True
-        
+
+        # Does first column have zero?
         for j in range(n):
             if matrix[0][j] == 0:
                 col_zero = True
 
+        # find zeroes and store the info in first row and column
         for i in range(1, m):
             for j in range(1, n):
                 if matrix[i][j] == 0:
                     matrix[i][0] = matrix[0][j] = 0
 
+        # set zeroes except the first row and column
         for i in range(1, m):
             for j in range(1, n):
                 if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
-
+                    
+        # set zeroes for first row and column if needed
         if col_zero:
             for j in range(n):
                 matrix[0][j] = 0
